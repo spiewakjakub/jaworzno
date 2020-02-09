c3c7774f530d3bfbce846de038d3df4a4b0cedfd
bkonczyk <bartkon@gmail.com>
2020-02-09 20:21:25 +0100

    saving work

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -14,6 +14,7 @@ import { FooterComponent } from './layouts/footer/footer.component';
 import { PageRibbonComponent } from './layouts/profiles/page-ribbon.component';
 import { ActiveMenuDirective } from './layouts/navbar/active-menu.directive';
 import { ErrorComponent } from './layouts/error/error.component';
+import { LogoBarComponent } from './layouts/logo-bar/logo-bar.component';
 
 @NgModule({
   imports: [
@@ -25,7 +26,15 @@ import { ErrorComponent } from './layouts/error/error.component';
     JaworznoEntityModule,
     JaworznoAppRoutingModule
   ],
-  declarations: [MainComponent, NavbarComponent, ErrorComponent, PageRibbonComponent, ActiveMenuDirective, FooterComponent],
+  declarations: [
+    MainComponent,
+    NavbarComponent,
+    ErrorComponent,
+    PageRibbonComponent,
+    ActiveMenuDirective,
+    FooterComponent,
+    LogoBarComponent
+  ],
   bootstrap: [MainComponent]
 })
 export class JaworznoAppModule {}
