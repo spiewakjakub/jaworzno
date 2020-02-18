1960896b5a13625457155712906f778c9708ea83
Jakub Śpiewak <jakubspiewak@outlook.com>
2020-02-18 15:14:29 +0100

    add annotatnion type byte[]

--- src/main/java/pl/jspiewak/domain/Slider.java
+++ src/main/java/pl/jspiewak/domain/Slider.java
@@ -1,6 +1,8 @@
 package pl.jspiewak.domain;
 
 
+import org.hibernate.annotations.Type;
+
 import javax.persistence.*;
 
 import java.io.Serializable;
@@ -20,6 +22,7 @@ public class Slider implements Serializable {
     private Long id;
 
     @Lob
+    @Type(type = "byte[]")
     @Column(name = "picture")
     private byte[] picture;
 
