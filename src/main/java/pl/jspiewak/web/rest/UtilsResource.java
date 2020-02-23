cd078066761a955388c4bdc3d77454c5c8ba3d47
Jakub Śpiewak <jakubspiewak@outlook.com>
2020-02-24 00:27:49 +0100

    temp rest function

--- src/main/java/pl/jspiewak/web/rest/UtilsResource.java
+++ src/main/java/pl/jspiewak/web/rest/UtilsResource.java
@@ -1,5 +1,6 @@
 package pl.jspiewak.web.rest;
 
+import com.zaxxer.hikari.HikariDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ClassPathResource;
@@ -9,9 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.sql.DataSource;
 import java.io.IOException;
 
-import static org.apache.commons.lang3.ArrayUtils.*;
+import static org.apache.commons.lang3.ArrayUtils.nullToEmpty;
 
 @RestController
 @RequestMapping("/api/util")
@@ -21,6 +23,13 @@ public class UtilsResource {
 
     private final Resource mainLogoResource = new ClassPathResource("images/main_logo.png");
     private final Resource cycleInfoResource = new ClassPathResource("images/cycle_info.jpg");
+
+    final
+    DataSource dataSource;
+
+    public UtilsResource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @GetMapping(value = "/logo", produces = MediaType.IMAGE_PNG_VALUE)
     public byte[] getMainLogo() throws IOException {
@@ -33,4 +42,12 @@ public class UtilsResource {
         log.debug("Request to get main logo");
         return nullToEmpty(cycleInfoResource.getInputStream().readAllBytes());
     }
+
+    @GetMapping(value = "/db")
+    public String getH2Connection() {
+        log.debug("Request to get main logo");
+        return ((HikariDataSource) dataSource).getJdbcUrl();
+    }
+
+
 }
